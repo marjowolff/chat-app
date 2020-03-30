da31e7028188aa446ccab37a80f16a226ab0166c
Marjolaine Baratte <marjolaine.baratte@gmail.com>
2020-03-30 17:45:42 +0200

    mise en forme

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,14 +1,17 @@
 import React from 'react'
 
-const onLine = false
+const onLine = true
 const Contact = () => (
     <div className='Contact'>
-        <img className='avatar' src = 'https://randomuser.me/api/portraits/men/84.jpg'/>
-        
-        <div className='status'>
-            <p className = 'name'> Gavin Martin </p>
-            <p className='status-online'></p>
-            <p className='status-text'>{onLine ? 'Online' : 'Offline'}</p>
+        <img className='avatar' src='https://randomuser.me/api/portraits/men/84.jpg' />
+        <div>
+            <h4 className='name'> Gavin Martin </h4>
+            
+            <div className='status'>
+            
+                <div className={onLine ? "status-online" : "status-offline"}></div>
+                <p className='status-text'>{onLine ? 'Online' : 'Offline'}</p>
+                </div>
         </div>
     </div>
 )
