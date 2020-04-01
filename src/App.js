2aef0e065481d3774fe138fb941a65e3d8ee4d75
Marjolaine Baratte <marjolaine.baratte@gmail.com>
2020-04-01 16:44:04 +0200

    Dynamisation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 
 import './Components/style.css';
-import Contact from './Components/Contact'
+import ContactList from './Components/ContactList'
 
 
 
 function App() {
   return (
     <div className="App">
-      <Contact name="Gavin Martin" avatar="https://randomuser.me/api/portraits/men/85.jpg" onLine/>
-      <Contact name="Timothee Jonhson" avatar="https://randomuser.me/api/portraits/men/1.jpg" onLine/>
-      <Contact name="Mildred Henderson" avatar="https://randomuser.me/api/portraits/women/90.jpg" 
-    />
+      <ContactList />
     </div>
   );
 }
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,17 +3,17 @@ import PropTypes from 'prop-types';
 
 
 const Contact = (props) => (
-  <div className="Contact">
-    <img
+  <div className="Contact" key={props.name}>
+    <img key="img"
       className="avatar"
-      src={props.avatar}
+      src={props.avatar} alt=""
     />
-    <div>
-      <h4 className="name"> {props.name} </h4>
+    <div key="div">
+      <h4 className="name" key="h4"> {props.name} </h4>
 
-      <div className="status">
-        <div className={props.onLine ? "status-online" : "status-offline"}></div>
-        <p className="status-text">{props.onLine ? "Online" : "Offline"}</p>
+      <div className="status" key="name">
+        <div key="div2" className={props.onLine ? "status-online" : "status-offline"}></div>
+        <p className="status-text" key={props.name} >{props.onLine ? "Online" : "Offline"}</p>
       </div>
     </div>
   </div>
