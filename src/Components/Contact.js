841f09f9c9766bf480d3f5fe80039ef310cea1b5
Marjolaine Baratte <marjolaine.baratte@gmail.com>
2020-04-06 11:05:00 +0200

    states

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,29 +1,46 @@
 import React from "react";
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 
-const Contact = (props) => (
-  <div className="Contact" key={props.name}>
+class Contact extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      onLine:props.onLine
+    }
+  }
+  
+  render () {
+
+    const statusChange = (e) => {
+      let valueTarget = this.state.onLine
+      this.setState ({onLine:!valueTarget})
+    }
+
+    return (<div className="Contact" key={this.props.name}>
     <img key="img"
       className="avatar"
-      src={props.avatar} alt=""
+      src={this.props.avatar} alt=""
     />
     <div key="div">
-      <h4 className="name" key="h4"> {props.name} </h4>
+      <h4 className="name" key="h4"> {this.props.name} </h4>
 
-      <div className="status" key="name">
-        <div key="div2" className={props.onLine ? "status-online" : "status-offline"}></div>
-        <p className="status-text" key={props.name} >{props.onLine ? "Online" : "Offline"}</p>
+      <div className="status" key="name" onClick={statusChange}>
+        <div key="div2" className={this.state.onLine ? "status-online" : "status-offline"}>
+        </div>
+        <p className="status-text" key={this.props.name} >{this.state.onLine ? "Online" : "Offline"}
+        </p>
       </div>
     </div>
-  </div>
-);
+  </div>)
+  }}
 
 
-Contact.propTypes = {
-  name : PropTypes.string.isRequired,
-  avatar : PropTypes.string.isRequired,
-  onLine : PropTypes.bool
-}
+
+// Contact.propTypes = {
+//   name : PropTypes.string.isRequired,
+//   avatar : PropTypes.string.isRequired,
+//   onLine : PropTypes.bool
+// }
 
 export default Contact;
